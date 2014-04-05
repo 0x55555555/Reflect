16ef2312cd920b8da108d70ab7db43cf48476dfb
Jorj <jorjpimm@gmail.com>
2014-04-05 22:11:02 +0100

    missed files, and updating readme

---
+++ include/Crate/EmbeddedTypes.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Crate/Type.h"
+
+namespace Crate
+{
+namespace detail
+{
+
+enum EmbeddedType
+  {
+  embedded_void,
+  embedded_bool,
+  embedded_int,
+  embedded_float,
+  embedded_double,
+
+  EmbeddedTypeCount
+  };
+
+#define EMBEDDED_TYPE(type) \
+  template <> struct TypeResolver<type> { \
+  static const Type *find() { static Type t(#type); return &t; } };
+
+EMBEDDED_TYPE(void)
+EMBEDDED_TYPE(bool)
+EMBEDDED_TYPE(int)
+EMBEDDED_TYPE(float)
+EMBEDDED_TYPE(double)
+
+#undef EMBEDDED_TYPE
+
+
+}
+}
---
+++ include/Crate/Type.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <type_traits>
+#include <string>
+
+namespace Crate
+{
+
+class Type
+  {
+public:
+  Type(const char *n) : m_name(n)
+    {
+    }
+
+  ~Type()
+    {
+    }
+
+  const std::string &name() const
+    {
+    return m_name;
+    }
+
+private:
+  std::string m_name;
+  };
+
+namespace detail
+{
+template <typename T> struct TypeResolver;
+
+template <typename T> struct TypeResolver<const T> : public TypeResolver<T> { };
+template <typename T> struct TypeResolver<T &> : public TypeResolver<T> { };
+template <typename T> struct TypeResolver<T *> : public TypeResolver<T> { };
+}
+
+/// \brief Find a type for a specified class. This will fail to compile if
+///        a TypeResolver is not defined for the type.
+template <typename T> inline const Type *findType()
+  {
+  typedef detail::TypeResolver<T> Resolver;
+
+  return Resolver::find();
+  }
+
+}
